4e6ca53672a2f2d6288ebb3400fe89d24b562e82
Patrik Gustavsson <patrik@beet.se>
2025-01-30 16:23:11 +0100

    chore: add ESLint and TypeScript configuration for improved code quality

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "vue-eslint-parser",
+  parserOptions: { parser: "@typescript-eslint/parser", sourceType: "module" },
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "no-unused-vars": "warn"
+  }
+};
