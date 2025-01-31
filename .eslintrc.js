4b1f4460e251b51f20378b95c0f4dc024a8f7487
Patrik Gustavsson <patrik@beet.se>
2025-01-31 11:09:58 +0100

    chore(linting): update ESLint configuration with explicit plugin declarations

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "plugin:@typescript-eslint/recommended"],
   parser: "vue-eslint-parser",
   parserOptions: { parser: "@typescript-eslint/parser", sourceType: "module" },
+  plugins: ["vue", "@typescript-eslint"],
   rules: {
     "vue/multi-word-component-names": "off",
     "@typescript-eslint/no-explicit-any": "warn",
